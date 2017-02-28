60dfff4193d4344a9cf92677147aa9c33adbf1ec
shadowkyros <randall.vowles@utah.edu>
2017-02-28 09:28:39 -0700

    whole table made (sort of)

--- elev_table.js
+++ elev_table.js
@@ -2,89 +2,87 @@
     "use strict";
 
 
+    var allFileURLs = [];
+    var i;
+    for (i = 1; i < 218; i++) {
+        var baseUrl = "http://home.chpc.utah.edu/~u0540701/metadata_correction/";
+        var fileName = "stations_to_investigate_network_" + String(i) + ".json";
+        allFileURLs[i] = baseUrl + fileName;
+        }
+    var state = {
+        baseUrl: 'http://home.chpc.utah.edu/~u0540701/metadata_correction/',
+        thisJSONFile: "stations_to_investigate_network_153.json"
+    }
 
-    // var i;
-    // var json_total = {};
-    // function _getJSON (){
-    //     for(i=1; i < 218; i++) {
-    //     $.getJSON("http://home.chpc.utah.edu/~u0540701/metadata_correction/stations_to_investigate_network_" + i + ".json", function(json) {
-    //         json_total[i] = json;
-    //     })
-    // }
-    // };
-    // _getJSON();
-    // $.when(_getJSON()).done(function(){
-    //     _networkTableEmitter(json_total)
-    // });
-    function get(url, callback) {
+    // HTTPFetch(state.baseUrl + state.thisJSONFile, printResponse)
+    // HTTPFetch(state.baseUrl + state.thisJSONFile, _networkTableEmitter)
+    allFileURLs.map(function(d){
+        HTTPFetch(d, _networkTableEmitter)
+    })
+
+    function HTTPFetch(url, callback) {
         var request = new XMLHttpRequest();
         request.open("GET", url)
         request.onreadystatechange = function () {
             if (request.readyState === 4 && request.status === 200) {
-                var type = request.getResponseHeader("Content-Type");
-                if (type.indexOf("XML") !== -1 && request.responseXML) {
-                    callback(request.responseXML);
-                } else if (type === "application/json") {
-                    callback(JSON.parse(request.responseText));
-                    var json_total = (JSON.parse(request.responseText));
-                } else {
-                    callback(request.responseText);
-                }
+
+                callback(JSON.parse(request.responseText))
             }
         }
+        request.send(null);
     }
-    get("http://home.chpc.utah.edu/~u0540701/metadata_correction/stations_to_investigate_network_153.json", console.log(request.responseText))
-    // var myRequest = new Request("http://home.chpc.utah.edu/~u0540701/metadata_correction/stations_to_investigate_network_153.json");
-    // fetch(myRequest)
-    //     .then(function(response){
-    //         return response.blob();
-    //     })
-    //     .then(function(response){
-    //         var json_total = JSON.parse(myRequest.responseText)
-    //     })
-    // $.when(fetch(myRequest)).done(function(){
-    //     _networkTableEmitter(json_total)
-    // });
-    
-    
-    
-    return;
+
+    function printResponse(a) {
+        console.log('URL To Fetch')
+        console.log(state.baseUrl + state.thisJSONFile)
+        console.log(a)
+    }
 
     /**
      * 
-     * @param {object} json_total
+     * @param {object} json
      * @param {object} args - Table configuration arguments
      */
-    function _networkTableEmitter(json_total) {
+    function _networkTableEmitter(json) {
+        // console.log(json)
+        // var rankedSensors = [
+        //     "api_elev", "delta_api", "latitude", "longitude",
+        //     "resolved_method_used", "resolved_elev", "stid"
+        // ];
+        // var headerName = [
+        //     "API Elevation", "Delta Elevation", "Latitude", "Longitude",
+        //     "Resolved Method Used", "Resolved Elevation", "STID", "LINK"
+        // ];
         var rankedSensors = [
-            "api_elev", "delta_api", "latitude", "longitude",
-            "resolved_method_used", "resolved_elev", "stid"
+            "stid", "latitiude", "longitude", "api_elev", "delta_api",
+            "resolve_method_used", "resolved_elev", "urlLink"
         ];
         var headerName = [
-            "API Elevation", "Delta Elevation", "Latitude", "Longitude",
-            "Resolved Method Used", "Resolved Elevation", "STID", "LINK"
+            "STID", "Latitude", "Longitude", "API Elevation", "Delta Elevation", 
+            "Resolved Method Used", "Resolved Elevation", "LINK"
         ];
 
-        var tableArgs = {
+        var googleURL = "http://maps.google.com/maps?z=12&t=k&q=loc:"
+
+        var args = {
             table_container: "#nettable-container",
             table_id: "nettable",
             table_class: "",
             sensors: rankedSensors
         };
+        // var json1 = JSON.parse(json)
         var stations = [];
-        var i = 0;
-        var l = json_total.length;
-        while (i < l) {
+        (Object.keys(json)).map(function (d) {
+            json[d]["urlLink"] = googleURL + String(json[d]["latitiude"]) + "+" + String(json[d]["longitude"])
             var tmp = {};
-            rankedSensors.map(function (d) {
-                tmp[d] = json_total[i][d];
-            });
+            tmp = json[d];
             stations.push(tmp);
-            i++;
-        }
-
+            
+        })
+        console.log(stations)
+        // console.log(json.length)
         // Create and append table to DOM, but first check to see if we have a table node.
-        d3.select("body " + args.table_container).selectAll("table").remove();
+        // d3.select("body " + args.table_container).selectAll("table").remove();
         var table = d3.select("body " + args.table_container).append("table")
             .attr("id", args.table_id);
 
@@ -94,20 +92,20 @@
             .html(function (d) {
                 return d;
             })
-            .property("sorted", false)
-            .on("click", function (d) {
-                if (d !== "STID") {
-                    return;
-                }
-                var _this = this;
-                var _state = d3.select(this).property("sorted");
-                d3.select(_this).property("sorted", function (d) {
-                    return _state ? false : true;
-                });
-                rows.sort(function (a, b) {
-                    return _state ? b.STID.localeCompare(a.STID) : a.STID.localeCompare(b.STID);
-                });
-            });
+        // .property("sorted", false)
+        // .on("click", function (d) {
+        //     if (d !== "STID") {
+        //         return;
+        //     }
+        //     var _this = this;
+        //     var _state = d3.select(this).property("sorted");
+        //     d3.select(_this).property("sorted", function (d) {
+        //         return _state ? false : true;
+        //     });
+        //     rows.sort(function (a, b) {
+        //         return _state ? b.STID.localeCompare(a.STID) : a.STID.localeCompare(b.STID);
+        //     });
+        // });
 
         // Create the rows
         var rows = table.append("tbody").selectAll("tr").data(stations).enter().append("tr");
@@ -125,6 +123,14 @@
             .enter().append("td")
             .text(function (d) {
                 return d.value;
-            });
+                // console.log("here we are2")
+            })
+            .attr("id", function (d) {
+                return d.name;
+            })
+        // var hyperlink = d3.selectAll("urlLink")
+        //     .on("click", function () {
+        //         window.open(d3.select(this).value());
+        //     });
     }
 })();
